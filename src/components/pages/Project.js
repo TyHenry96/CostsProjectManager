c2c44b0efcc5d0941e7f6126bfd03c889e5ac075
TyHenryDev <henriquemt.contato@gmail.com>
2023-02-24 14:28:09 -0300

    update3

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -98,13 +98,34 @@ function Project() {
         })
         .then((resp) => resp.json())
         .then((data) => {
-            //exibir os serviços
-            console.log(data)
+            setShowServiceForm(false)
         })
         .catch(err => console.log(err))
     }
 
-    function removeService(){
+    function removeService(id, cost){
+        
+        const servicesUpdated = project.services.filter(
+            (service) => service.id !== id
+        )
+        const projectUpdated = project
+        projectUpdated.services = servicesUpdated
+        projectUpdated.cost = parseFloat(projectUpdated.cost) - parseFloat(cost)
+
+        fetch(`http://localhost:5000/projects/${projectUpdated.id}`,{
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(projectUpdated)
+        }).then((resp) => resp.json())
+        .then((data) => {
+            setProject(projectUpdated)
+            setServices(servicesUpdated)
+            setMessage(`Serviço removido com sucesso!`)
+
+        })
+        .catch(err => console.log(err))
 
     }
 
